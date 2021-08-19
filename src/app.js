0665b6a5baea1445c75da7050f73d2f5e219ce2e
Alejandro Nuñez <john.alejandro@tresastronautas.com>
2021-08-18 20:20:16 -0500

    Actividad 1 - Resolucion duda leer tamaño de pantalla

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 window.onscroll = () => {
   const fakeH = document.querySelector('#fakeHeader');
-  if (window.pageYOffset >= 150) {
-    fakeH.style.top = '0px';
-    fakeH.style.opacity = '1';
+
+  if (window.innerWidth > 375) {
+    // Escritorio
+    if (window.pageYOffset >= 150) {
+      fakeH.style.top = '0px';
+      fakeH.style.opacity = '1';
+    } else {
+      fakeH.style.top = '-75px';
+      fakeH.style.opacity = '0';
+    }
   } else {
-    fakeH.style.top = '-75px';
-    fakeH.style.opacity = '0';
+    // Movil
   }
 };
